1b7aba8f14de5a2d1aba9aecc74f920c1464b977
Florine Lefebvre <florine.lefebvre.etu@univ-lille.fr>
2025-07-03 17:26:32 +0200

    ajout du menu et du juste prix

---
+++ games/juste-prix.js
@@ -0,0 +1,34 @@
+let correctAnswer = Math.floor(Math.random() * 100) + 1;
+let attemptNumber = 0;
+
+const guessButton = document.querySelector('#guessButton');
+const guessInput = document.querySelector('#guessInput');
+const guessWindow = document.querySelector('#guess');
+
+guessButton.addEventListener('click', guess);
+
+function guess(event) {
+	let answer = guessInput.value;
+	//verification de la validité de la réponse
+	if (answer > 0 && answer <= 100) {
+		// incrémentation du compteur d'essai
+		attemptNumber++;
+		if (attemptNumber === 0 || attemptNumber === 1) {
+			document.querySelector('#attemptNumber').innerHTML =
+				`Nombre de tentative : ${attemptNumber}`;
+		} else {
+			document.querySelector('#attemptNumber').innerHTML =
+				`Nombre de tentatives : ${attemptNumber}`;
+		}
+
+		//vérification de la réponse donnée
+		if (answer < correctAnswer) {
+			guessWindow.innerHTML = `${answer} ⬆️`;
+		} else if (answer > correctAnswer) {
+			guessWindow.innerHTML = `${answer} ⬇️`;
+		} else {
+			guessWindow.innerHTML = `${answer} ✅`;
+			guessButton.disabled = true;
+		}
+	}
+}
---
+++ idées-de-jeux.md
@@ -0,0 +1,9 @@
+1. juste prix
+1. brownie clicker
+1. memory
+1. snake
+1. reflexes
+1. wordle
+1. labyrinthe
+1. petit bac
+1. souris
